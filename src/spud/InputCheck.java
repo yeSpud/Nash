f57c4e73f9f4e408c89e36d05e1d57b867873e6d
Spud <jeffrypig23@gmail.com>
2022-12-30 17:47:57 -0800

    Created new input validation

--- src/spud/InputCheck.java
+++ src/spud/InputCheck.java
@@ -4,31 +4,20 @@ import javafx.scene.control.TextFormatter;
 
 import java.util.function.UnaryOperator;
 
+@Deprecated
 public class InputCheck implements UnaryOperator<TextFormatter.Change> {
 
-	private static final String REGEX = "[^0-9]";
+	private static final String allowedChars = "^-|[\\.0-9]";
 
 	/**
-	 * Credit to karimsqualli96 on GitHub
-	 * https://gist.github.com/karimsqualli96/f8d4c2995da8e11496ed
+	 * Credit to <a href="https://gist.github.com/karimsqualli96/f8d4c2995da8e11496ed">karimsqualli96</a> on GitHub.
 	 */
 	@Override
 	public TextFormatter.Change apply(TextFormatter.Change change) {
 
-		if (change.isReplaced()) {
-			if (change.getText().matches(InputCheck.REGEX)) {
+		if (change.isReplaced() || change.isAdded()) {
+			if (!change.getText().matches(InputCheck.allowedChars)) {
 				change.setText(change.getControlText().substring(change.getRangeStart(), change.getRangeEnd()));
-			}
-		}
-
-
-		if (change.isAdded()) {
-			if (change.getControlText().contains(".")) {
-				if (change.getText().matches(InputCheck.REGEX)) {
-					change.setText("");
-				}
-			} else if (change.getText().matches("[^0-9.]")) {
-				change.setText("");
 			}
 		}
 
--- src/spud/Nash.java
+++ src/spud/Nash.java
@@ -1,13 +1,16 @@
 package spud;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.control.TextFormatter;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -20,25 +23,17 @@ public class Nash extends Application {
 		Nash.launch(args);
 	}
 
-	/**
-	 * TODO Documentation
-	 */
-	public static Button button;
+	public Button button;
 
-	/**
-	 * TODO Documentation
-	 */
-	public static TextField LeftUpA, LeftUpB, RightUpA, RightUpB, LeftDownA, LeftDownB, RightDownA, RightDownB;
+	public TextField LeftUpA, LeftUpB, RightUpA, RightUpB, LeftDownA, LeftDownB, RightDownA, RightDownB;
 
-	/**
-	 * TODO Documentation
-	 */
-	protected static final Calculate CALCULATE = new Calculate();
+	public TextField[] AllFields;
 
-	/**
-	 * TODO Documentation
-	 */
-	protected static final Reset RESET = new Reset();
+	private final Calculate CalculateOperation = new Calculate();
+
+	private final Reset ResetOperation = new Reset();
+
+	private byte CompletedCells = 0;
 
 	@Override
 	public void start(Stage primaryStage) {
@@ -63,52 +58,161 @@ public class Nash extends Application {
 			switch (id) {
 
 				case "Button" -> {
-					Nash.button = (Button) element;
-					Nash.button.setText("Calculate");
-					Nash.button.setOnAction(Nash.CALCULATE);
+					button = (Button) element;
+					initButton();
 				}
-
 				case "LeftUpA" -> {
-					Nash.LeftUpA = (TextField) element;
-					Nash.LeftUpA.setTextFormatter(new TextFormatter<>(new InputCheck()));
+					LeftUpA = (TextField) element;
+					LeftUpA.textProperty().addListener(new InputValidation());//.setOnAction(new InputValidation());
+					/*
+					LeftUpA.setTextFormatter(new TextFormatter<>(changed -> {
+
+						if (changed.isContentChange()) {
+							if (!changed.getText().matches("\\d*")) {
+								changed.setText(changed.getControlText().substring(changed.getRangeStart(), changed.getRangeEnd()));
+							}
+						}
+						return changed;
+					}));*/
+					//LeftUpA.setTextFormatter(new TextFormatter<>(new InputCheck()));
 				}
 				case "LeftUpB" -> {
-					Nash.LeftUpB = (TextField) element;
-					Nash.LeftUpB.setTextFormatter(new TextFormatter<>(new InputCheck()));
+					LeftUpB = (TextField) element;
+					LeftUpB.textProperty().addListener(new InputValidation());
+					//LeftUpB.setTextFormatter(new TextFormatter<>(new InputCheck()));
 				}
 				case "RightUpA" -> {
-					Nash.RightUpA = (TextField) element;
-					Nash.RightUpA.setTextFormatter(new TextFormatter<>(new InputCheck()));
+					RightUpA = (TextField) element;
+					RightUpA.textProperty().addListener(new InputValidation());
+					//RightUpA.setTextFormatter(new TextFormatter<>(new InputCheck()));
 				}
 				case "RightUpB" -> {
-					Nash.RightUpB = (TextField) element;
-					Nash.RightUpB.setTextFormatter(new TextFormatter<>(new InputCheck()));
+					RightUpB = (TextField) element;
+					RightUpB.textProperty().addListener(new InputValidation());
+					//RightUpB.setTextFormatter(new TextFormatter<>(new InputCheck()));
 				}
 				case "LeftDownA" -> {
-					Nash.LeftDownA = (TextField) element;
-					Nash.LeftDownA.setTextFormatter(new TextFormatter<>(new InputCheck()));
+					LeftDownA = (TextField) element;
+					LeftDownA.textProperty().addListener(new InputValidation());
+					//LeftDownA.setTextFormatter(new TextFormatter<>(new InputCheck()));
 				}
 				case "LeftDownB" -> {
-					Nash.LeftDownB = (TextField) element;
-					Nash.LeftDownB.setTextFormatter(new TextFormatter<>(new InputCheck()));
+					LeftDownB = (TextField) element;
+					LeftDownB.textProperty().addListener(new InputValidation());
+					//LeftDownB.setTextFormatter(new TextFormatter<>(new InputCheck()));
 				}
 				case "RightDownA" -> {
-					Nash.RightDownA = (TextField) element;
-					Nash.RightDownA.setTextFormatter(new TextFormatter<>(new InputCheck()));
+					RightDownA = (TextField) element;
+					RightDownA.textProperty().addListener(new InputValidation());
+					//RightDownA.setTextFormatter(new TextFormatter<>(new InputCheck()));
 				}
 				case "RightDownB" -> {
-					Nash.RightDownB = (TextField) element;
-					Nash.RightDownB.setTextFormatter(new TextFormatter<>(new InputCheck()));
+					RightDownB = (TextField) element;
+					RightDownB.textProperty().addListener(new InputValidation());
+					//RightDownB.setTextFormatter(new TextFormatter<>(new InputCheck()));
 				}
 			}
-
 		}
+
+		AllFields = new TextField[]{LeftUpA, LeftUpB, RightUpA, RightUpB, LeftDownA, LeftDownB, RightDownA, RightDownB};
 
 		Scene scene = new Scene(parent);
 
+		primaryStage.setResizable(false);
 		primaryStage.setTitle("Nash");
 		primaryStage.setScene(scene);
 		primaryStage.show();
 
+	}
+
+	private void initButton() {
+		button.setText("Calculate");
+		button.setOnAction(CalculateOperation);
+		button.setDisable(true);
+	}
+
+	private class InputValidation implements ChangeListener<String> {
+
+		private int lastUpdated = 0;
+
+		@Override
+		public void changed(ObservableValue<? extends String> observableValue, String oldValue, String newValue) {
+
+			if (newValue.isEmpty()) {
+				if (lastUpdated > -1) {
+					--CompletedCells;
+					lastUpdated = -1;
+				}
+			} else {
+				if (lastUpdated < 1) {
+					++CompletedCells;
+					lastUpdated = 1;
+				}
+			}
+			button.setDisable(CompletedCells != 8);
+
+		}
+	}
+
+	private class Calculate implements EventHandler<ActionEvent> {
+
+		private static final String CORRECT = "-fx-text-fill: green", INCORRECT = "-fx-text-fill: red;";
+
+		@Override
+		public void handle(ActionEvent event) {
+
+			findCorrect(LeftUpA, LeftDownA);
+			findCorrect(RightUpA, RightDownA);
+			findCorrect(RightUpB, LeftUpB);
+			findCorrect(RightDownB, LeftDownB);
+
+
+			for (TextField textField : AllFields) {
+				textField.setDisable(true);
+			}
+
+			button.setText("Reset");
+			button.setOnAction(ResetOperation);
+		}
+
+		private void findCorrect(TextField a, TextField b) {
+
+			String aInput = a.getText(), bInput = b.getText();
+			if (aInput.isEmpty() || bInput.isEmpty()) {
+				return;
+			}
+
+			try {
+				if (Double.parseDouble(aInput) > Double.parseDouble(bInput)) {
+					a.setStyle(CORRECT);
+					b.setStyle(INCORRECT);
+				} else if (Double.parseDouble(aInput) < Double.parseDouble(bInput)) {
+					a.setStyle(INCORRECT);
+					b.setStyle(CORRECT);
+				} else {
+					a.setStyle(CORRECT);
+					b.setStyle(CORRECT);
+				}
+			} catch (NumberFormatException notNumber) {
+				a.clear();
+				b.clear();
+			}
+		}
+
+	}
+
+	private class Reset implements EventHandler<ActionEvent> {
+
+		@Override
+		public void handle(ActionEvent event) {
+
+			for (TextField textField : AllFields) {
+				textField.clear();
+				textField.setStyle("");
+				textField.setDisable(false);
+			}
+
+			initButton();
+		}
 	}
 }
